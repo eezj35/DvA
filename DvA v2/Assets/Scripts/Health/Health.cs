374abacd43be6a4ba66759d44344b69af74acba8
eezj35 <justineezj0@gmail.com>
2021-12-22 13:41:17 +0800

    Readjust main camera field of view Add drump hurt animation and logic

--- DvA v2/Assets/Scripts/Health/Health.cs
+++ DvA v2/Assets/Scripts/Health/Health.cs
@@ -27,4 +27,12 @@ public class Health : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
+        }
+    }
+
 }
